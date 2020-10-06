1185bf083832d11f31ea7f75dd13fec3ad6e8aab
zhanglun <zhanglun1410@gmail.com>
2020-10-06 11:12:18 +0800

    :white_check_mark: fix test action

--- app/app.test.tsx
+++ app/app.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './app';
 
-test('renders learn react link', () => {
+test('renders Feed Heading', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = getByText(/Feed/i);
+
+  expect(linkElement).toBeDefined();
 });
