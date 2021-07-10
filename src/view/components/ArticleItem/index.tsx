5e0bc988440f917f6d6fae9d13c3387054eaa115
zhanglun <zhanglun1410@gmail.com>
2021-07-10 09:39:28 +0800

    improvement: add expand content

--- src/view/components/ArticleItem/index.tsx
+++ src/view/components/ArticleItem/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import Dayjs from 'dayjs';
 import { ArticleReadStatus } from '../../../infra/constants/status';
 import { Article } from '../../../infra/types';
@@ -10,17 +10,24 @@ type ArticleItemProps = {
 
 export const ArticleItem = (props: ArticleItemProps) => {
   const { article } = props;
-  const handleClick = () => {};
+  const [expand, setExpand] = useState(false);
+  const handleClick = useCallback(() => {
+    setExpand(!expand);
+  }, [expand]);
 
   return (
     <li
       className={`${styles.item} ${
         article.hasRead === ArticleReadStatus.isRead && styles.read
-      }`}
+      } ${expand && styles.expand}`}
       onClick={handleClick}
       aria-hidden="true"
     >
       <div className={styles.title}>{article.title}</div>
+      <div
+        className={styles.content}
+        dangerouslySetInnerHTML={{ __html: article.content }}
+      />
       {/* <div className={styles.meta}>
         <span className={styles.channel}>{article.channelTitle}</span>
         <span className={styles.pubTime}>
