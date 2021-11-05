cb0616b973c9162c4ef896e9a6280a41b6bcd1df
zhanglun <zhanglun1410@gmail.com>
2021-11-05 13:17:41 +0800

    improvement: :lipstick: setting panel

--- src/view/components/SettingPanel/index.tsx
+++ src/view/components/SettingPanel/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tabs, TabPane } from '@douyinfe/semi-ui';
 import { SettingSubscribe } from './Subscribe';
 import { ImportAndExport } from './ImportAndExport';
 import { MainHeader } from '../MainHeader';
@@ -9,8 +10,15 @@ function SettingPanel() {
     <div className={styles.container}>
       <MainHeader title="设置" />
       <div className={styles.panelContainer}>
-        <SettingSubscribe />
-        <ImportAndExport />
+        <Tabs type="line">
+          <TabPane tab="订阅管理" itemKey="1">
+            TODO
+          </TabPane>
+          <TabPane tab="导入/导出" itemKey="2">
+            <SettingSubscribe />
+            <ImportAndExport />
+          </TabPane>
+        </Tabs>
       </div>
     </div>
   );
