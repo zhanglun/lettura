e914de55b87801c71be6f8a24d989bbab682aebf
zhanglun <zhanglun1410@gmail.com>
2022-09-01 16:45:20 +0800

    feat: customize window title bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import React, {useState} from "react";
-import {
-  Outlet
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { appWindow } from '@tauri-apps/api/window'
+import { Outlet } from "react-router-dom";
 import { StoreContext } from "./context";
 import { ChannelList } from "./components/ChannelList";
-import styles from "./App.module.css";
-import "./styles/index.global.css";
-import "./App.css";
 import { useStore } from "./hooks/useStore";
+import styles from "./App.module.css";
+import "./styles/index.global.scss";
+import "./App.css";
 
 function App() {
   const store = useStore();
   const [filter, setFilter] = useState({...store.currentFilter});
 
+  useEffect(() => {
+    document
+      .getElementById('titlebar-minimize')
+      ?.addEventListener('click', () => appWindow.minimize())
+    document
+      .getElementById('titlebar-maximize')
+      ?.addEventListener('click', () => appWindow.toggleMaximize())
+    document
+      .getElementById('titlebar-close')
+      ?.addEventListener('click', () => appWindow.close())
+  })
   return (
     <StoreContext.Provider value={{
       channel: store.channel,
