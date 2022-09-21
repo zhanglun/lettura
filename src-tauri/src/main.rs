da0be05933c2314739639b38df0b35b42e44240c
zhanglun <zhanglun1410@gmail.com>
2022-09-21 21:12:47 +0800

    test: add window menus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -37,20 +37,26 @@ pub fn get_menu() -> Menu {
       "Selected and disabled",
     ))
     .add_native_item(MenuItem::Separator)
-    .add_item(test_item);
+    .add_native_item(MenuItem::Quit)
+    .add_item(test_item)
+    .add_item(disable_item);
 
-    let edit_menu = Menu::new()
-      .add_native_item(MenuItem::SelectAll)
-      .add_native_item(MenuItem::Copy)
-      .add_native_item(MenuItem::Paste)
-      .add_native_item(MenuItem::Separator)
-      .add_native_item(MenuItem::EnterFullScreen);
+  let edit_menu = Menu::new()
+    .add_native_item(MenuItem::SelectAll)
+    .add_native_item(MenuItem::Copy)
+    .add_native_item(MenuItem::Paste)
+    .add_native_item(MenuItem::Separator)
+    .add_native_item(MenuItem::EnterFullScreen);
+
+  let window_menu = Menu::new()
+    .add_native_item(MenuItem::Hide);
 
   // add all our childs to the menu (order is how they'll appear)
   Menu::new()
     // .add_submenu(Submenu::new("My app", my_app_menu))
-    .add_submenu(Submenu::new("Other menu", test_menu))
+    .add_submenu(Submenu::new("Other menu 2", test_menu))
     .add_submenu(Submenu::new("Edit", edit_menu))
+    .add_submenu(Submenu::new("Window", window_menu))
 }
 
 fn main() {
