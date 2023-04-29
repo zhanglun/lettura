483d74a1dc636d4de622327e65f7516b588f92c5
zhanglun <zhanglun1410@gmail.com>
2023-04-29 11:21:34 +0800

    feat: parse item correctly

--- happyhues/index.mjs
+++ happyhues/index.mjs
@@ -1,37 +1,62 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 let cache = new Map();
 let res = new Map();
 
-function parseSection (dom) {
-  const heads = dom.querySelectorAll('h4');
-  const hues = dom.querySelectorAll('.hues-wrap');
-  console.log("%c Line:8 🥔 hues", "color:#b03734", hues);
 
-  heads.forEach((head, idx) => {
-    const block = head.innerText.toLowerCase().trim();
-    hues[idx].querySelectorAll('.section-hues-row').forEach(($row) => {
-      let [title, value] = $row.innerText.split('\n');
-      const key = ['color', block, title.toLowerCase().trim().replace(/\s/, '-')]
+function parseSection ($, sectionId, dom) {
+  const prefix = `color-${sectionId}`;
+  const heads = dom.find('h4');
 
-      if (cache.get(key)) {
-        key.push(cache.get(key) + 1)
-      } else {
-        cache.set(key, 1)
-      }
+  if (heads.length === 0) {
+    dom.find('.section-hues-row').each((i, row) => {
+      let title = $(row).find('.hue-title').text().toLowerCase().trim().replace(/\s/ig, '-');
+      let value = $(row).find('.hue-hex').text().trim();
+      let key = [prefix, title].join('-');
 
       res.set(key, value.trim())
     })
-  });
+  } else {
+    heads.each((idx, head) => {
+      const $hues = $(head).nextUntil('h4');
+      const block = $(head).text().toLowerCase().trim();
+
+      $hues.find('.section-hues-row').each((i, row) => {
+        let title = $(row).find('.hue-title').text().toLowerCase().trim().replace(/\s/ig, '-');
+        let value = $(row).find('.hue-hex').text().trim();
+        let key = [prefix, block, title].join('-');
+
+        // if (cache.get(key)) {
+        //   cache.set(key, cache.get(key) + 1)
+        //   key = `${key}-${cache.get(key)}`
+        // } else {
+        //   cache.set(key, 1)
+        // }
+        res.set(key, value.trim())
+      })
+    });
+  }
 }
 
-document.querySelectorAll('.section-hues-wrap').forEach(($wrap) => {
-  parseSection($wrap);
-})
+const fetchPages = async () => {
+  try {
+    const response = await axios.get('https://www.happyhues.co/palettes/14');
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-let json = {};
-for (val of res) {
-  console.log("%c Line:33 🍭 val", "color:#33a5ff", val);
-  json[val[0].join('-')] = val[1];
+    $('.section.wf-section').each((i, wrap) => {
+      console.log("🚀 ~ file: index.mjs:40 ~ $ ~ i:", i)
+      parseSection($, i + 1, $(wrap));
+    });
+
+    let json = {};
+
+    console.log("🚀 ~ file: index.mjs:33 ~ res:", res)
+  } catch (err) {
+    throw err;
+  }
 }
 
-console.log("%c Line:34 🌰 json", "color:#7f2b82", json);
+// Print all tags in the console
+fetchPages().then(titles => console.log(titles));
