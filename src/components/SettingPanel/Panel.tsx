fddcc91678e2f424373ec03916a5138f30bd3168
zhanglun <zhanglun1410@gmail.com>
2023-05-19 15:15:58 +0800

    refactor: setting panel

--- src/components/SettingPanel/Panel.tsx
+++ src/components/SettingPanel/Panel.tsx
@@ -12,8 +12,8 @@ export const Panel = (props: PanelProps) => {
   return (
     <div className="">
       <div className="mb-5">
-        <h2 className="text-2xl font-semibold tracking-tight mb-4">{title}</h2>
-        {subTitle && <p className="text-sm mb-3">{subTitle}</p>}
+        <h2 className="text-2xl font-semibold tracking-tight mb-6">{title}</h2>
+        {subTitle && <p className="text-sm mb-3 text-[hsl(var(--foreground)_/_0.6)]">{subTitle}</p>}
       </div>
       {children}
     </div>
@@ -26,10 +26,12 @@ export const PanelSection = (props: PanelSectionProps) => {
   const { title, subTitle, children } = props;
 
   return (
-    <div className="pb-5 mb-5 border-b">
-      <h3 className="text-base font-semibold tracking-tight mb-2">{title}</h3>
-      {subTitle && <p className="text-sm mb-2">{subTitle}</p>}
-      {children}
+    <div className="flex items-center justify-between pb-4 mb-6 mt-4 border-b">
+      <div>
+        <h3 className="text-base font-semibold tracking-tight mb-2">{title}</h3>
+        {subTitle && <p className="text-sm mb-2 text-[hsl(var(--foreground)_/_0.6)]">{subTitle}</p>}
+      </div>
+      <div>{children}</div>
     </div>
   );
 };
--- src/components/SettingPanel/index.tsx
+++ src/components/SettingPanel/index.tsx
@@ -4,84 +4,84 @@ import classnames from "classnames";
 import { ArrowLeft, Cog, Database, Palette, Rss } from "lucide-react";
 import { RouteConfig } from "../../config";
 import styles from "./setting.module.scss";
+import { Icon } from "../Icon";
 
 function SettingPanel() {
   return (
     <div className={styles.container}>
-      <div className={`sticky-header ${styles.header}`}>
+      <div>
         <div
-          className={classnames(
-            styles.title,
-            "p-4 pl-5 font-bold text-detail-headline"
-          )}
+          className={"flex items-center p-4 font-semibold text-2xl"}
         >
-          <span className={styles.menuIcon}>
-            <ArrowLeft size={16} />
-          </span>
+          <Icon className="mr-3">
+            <ArrowLeft size={22} />
+          </Icon>
           Settings
         </div>
+        <div className="px-4 py-2">
+          <NavLink
+            to={RouteConfig.SETTINGS_GENERAL}
+            className={({ isActive }) =>
+              classnames(
+                "flex items-center px-2 h-10 rounded-md cursor-pointer mt-[2px]",
+                {
+                  "bg-primary text-primary-foreground font-bold": isActive,
+                  "hover:bg-primary hover:text-primary-foreground": !isActive,
+                }
+              )
+            }
+          >
+            <Cog size={16} className="mr-4" />
+            General
+          </NavLink>
+          <NavLink
+            to={RouteConfig.SETTINGS_APPEARANCE}
+            className={({ isActive }) =>
+              classnames(
+                "flex items-center px-2 h-10 rounded-md cursor-pointer mt-[2px]",
+                {
+                  "bg-primary text-primary-foreground font-bold": isActive,
+                  "hover:bg-primary hover:text-primary-foreground": !isActive,
+                }
+              )
+            }
+          >
+            <Palette size={16} className="mr-3" />
+            Appearance
+          </NavLink>
+          <NavLink
+            to={RouteConfig.SETTINGS_FEED_MANAGER}
+            className={({ isActive }) =>
+              classnames(
+                "flex items-center px-2 h-10 rounded-md cursor-pointer mt-[2px]",
+                {
+                  "bg-primary text-primary-foreground font-bold": isActive,
+                  "hover:bg-primary hover:text-primary-foreground": !isActive,
+                }
+              )
+            }
+          >
+            <Database size={16} className="mr-3" />
+            Content
+          </NavLink>
+          <NavLink
+            to={RouteConfig.SETTINGS_IMPORT}
+            className={({ isActive }) =>
+              classnames(
+                "flex items-center px-2 h-10 rounded-md cursor-pointer mt-[2px]",
+                {
+                  "bg-primary text-primary-foreground font-bold": isActive,
+                  "hover:bg-primary hover:text-primary-foreground": !isActive,
+                }
+              )
+            }
+          >
+            <Rss size={16} className="mr-3" />
+            Import/Export
+          </NavLink>
+        </div>
       </div>
-      <div className={styles.sidebar}>
-        <ul>
-          <li>
-            <NavLink
-              to={RouteConfig.SETTINGS_GENERAL}
-              className={({ isActive }) =>
-                classnames(styles.menuItem, "hover:text-primary", {
-                  "font-bold text-primary": isActive,
-                  "text-detail-paragraph": !isActive,
-                })
-              }
-            >
-              <Cog size={16} />
-              General
-            </NavLink>
-          </li>
-          {/* <li>
-            <NavLink
-              to={RouteConfig.SETTINGS_APPEARANCE}
-              className={({ isActive }) =>
-                classnames(styles.menuItem, "hover:text-primary", {
-                  "font-bold text-primary": isActive,
-                  "text-detail-paragraph": !isActive,
-                })
-              }
-            >
-              <Palette size={16} />
-              Appearance
-            </NavLink>
-          </li> */}
-          <li>
-            <NavLink
-              to={RouteConfig.SETTINGS_FEED_MANAGER}
-              className={({ isActive }) =>
-                classnames(styles.menuItem, "hover:text-primary", {
-                  "font-bold text-primary": isActive,
-                  "text-detail-paragraph": !isActive,
-                })
-              }
-            >
-              <Database size={16} />
-              Content
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={RouteConfig.SETTINGS_IMPORT}
-              className={({ isActive }) =>
-                classnames(styles.menuItem, "hover:text-primary", {
-                  "font-bold text-primary": isActive,
-                  "text-detail-paragraph": !isActive,
-                })
-              }
-            >
-              <Rss size={16} />
-              Import/Export
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.main}>
+      <div className="px-12 py-16">
         <Outlet />
       </div>
     </div>
