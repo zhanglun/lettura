44dc87d9b5e1356bba8788bdf25b755ea7b43d85
zhanglun <zhanglun1410@gmail.com>
2024-04-10 17:44:52 +0800

    chore: :arrow_up: update sooner@1.4.41

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,30 +1,32 @@
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { useBearStore } from "@/stores";
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const store = useBearStore((state) => ({
+    userConfig: state.userConfig,
+  }));
 
   return (
     <Sonner
       richColors={true}
-      theme={theme as ToasterProps["theme"]}
+      theme={store.userConfig.theme as ToasterProps["theme"]}
       className="toaster group"
-      toastOptions={{
-        classNames: {
-          toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
-        },
-      }}
+      // toastOptions={{
+      //   classNames: {
+      //     toast:
+      //       "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+      //     description: "group-[.toast]:text-muted-foreground",
+      //     actionButton:
+      //       "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+      //     cancelButton:
+      //       "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+      //   },
+      // }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
