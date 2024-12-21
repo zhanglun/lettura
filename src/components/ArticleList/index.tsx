73c229c76bf304d23d5ee9414a4e4d6b9dcc7f00
zhanglun <zhanglun1410@gmail.com>
2024-12-21 16:06:26 +0800

    improvement: format code

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -25,59 +25,62 @@ export interface ArticleListRefType {
   innerRef: React.RefObject<HTMLDivElement>;
 }
 
-export const ArticleList = React.memo(React.forwardRef<HTMLDivElement, any>((props: ArticleListProps, ref) => {
-  const { articles, isEmpty, isLoading, isReachingEnd, size, setSize } = props;
-  const loadRef = useRef<HTMLDivElement | null>(null);
-  const entry = useIntersectionObserver(loadRef, {});
-  const loadRefVisible = !!entry?.isIntersecting;
+export const ArticleList = React.memo(
+  React.forwardRef<HTMLDivElement, any>((props: ArticleListProps, ref) => {
+    const { articles, isEmpty, isLoading, isReachingEnd, size, setSize } = props;
+    const loadRef = useRef<HTMLDivElement | null>(null);
+    const entry = useIntersectionObserver(loadRef, {});
+    const loadRefVisible = !!entry?.isIntersecting;
 
-  const renderList = (): JSX.Element[] => {
-    return (articles || []).map((article: any, idx: number) => {
-      return (
-        <motion.div
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.2 }}
-          initial={{ opacity: 0, y: 30 }}
-          key={article.title + idx}
-          className="w-full"
-        >
-          <ArticleItem article={article} />
-        </motion.div>
-      );
-    });
-  };
+    const renderList = (): JSX.Element[] => {
+      return (articles || []).map((article: any, idx: number) => {
+        return (
+          <motion.div
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.2 }}
+            initial={{ opacity: 0, y: 30 }}
+            key={article.title + idx}
+            className="w-full"
+          >
+            <ArticleItem article={article} />
+          </motion.div>
+        );
+      });
+    };
 
-  useEffect(() => {
-    if (loadRefVisible && !isReachingEnd) {
-      setSize(size + 1);
-    }
-  }, [loadRefVisible, isReachingEnd]);
+    useEffect(() => {
+      if (loadRefVisible && !isReachingEnd) {
+        setSize(size + 1);
+      }
+    }, [loadRefVisible, isReachingEnd]);
 
-  return (
-    <div className="w-full" ref={ref}>
-      {isEmpty ? (
-        <div className="absolute top-1/2 -translate-y-1/2 w-full flex flex-col justify-center items-center gap-1 text-muted-foreground">
-          <Snail size={34} strokeWidth={1} />
-          <p>Yay, no matching items.</p>
-        </div>
-      ) : null}
-      <ul className="m-0 flex flex-col gap-[2px] pt-1 pr-0 pb-1 pl-1">{renderList()}</ul>
-      <div ref={loadRef} className="pt-1">
-        {isLoading && (
-          <div className="p-3 pl-6 grid gap-1 relative">
-            <Skeleton className="h-5 w-full" />
-            <div>
-              <Skeleton className="h-3 w-full" />
-            </div>
-            <div>
-              <Skeleton className="h-3 w-full m-[-2px]" />
-            </div>
-            <div>
-              <Skeleton className="h-3 w-32" />
-            </div>
+    return (
+      <div className="w-full" ref={ref}>
+        {isEmpty ? (
+          <div className="absolute top-1/2 -translate-y-1/2 w-full flex flex-col justify-center items-center gap-1 text-muted-foreground">
+            <Snail size={34} strokeWidth={1} />
+            <p>Yay, no matching items.</p>
           </div>
-        )}
+        ) : null}
+        <ul className="m-0 flex flex-col gap-[2px] pt-1 pr-0 pb-1 pl-1">{renderList()}</ul>
+        <div ref={loadRef} className="pt-1">
+          {isLoading && (
+            <div className="p-2 pl-6 grid gap-1 relative">
+              <Skeleton className="h-5 w-full" />
+              <div>
+                <Skeleton className="h-3 w-full" />
+              </div>
+              <div>
+                <Skeleton className="h-3 w-full" />
+              </div>
+              <div className="flex justify-between">
+                <Skeleton className="h-3 w-32" />
+                <Skeleton className="h-3 w-16" />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
-}));
+    );
+  })
+);
